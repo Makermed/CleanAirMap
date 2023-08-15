d0fd2f7570569a641a3a69ff87e26685864ff00d
Adam Glauser <adamglauser@gmail.com>
2023-08-15 00:08:44 -0400

    Bugfix: include RoomModel in Nexus types
    Missed including the RoomModel source type in the Nexus schema defintion

--- backend/src/api/schema.ts
+++ backend/src/api/schema.ts
@@ -13,7 +13,7 @@ export const schema = makeSchema({
     export: "Context",
   },
   sourceTypes: {
-    headers: [ 'import { LocationModel } from "./src/dataAccess/dataTypes" '],
+    headers: [ 'import { LocationModel, RoomModel } from "./src/dataAccess/dataTypes" '],
     modules: []
   }
 })
